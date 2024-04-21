16081e2f8626df57a98a048e206d69c2eab0554e
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2024-04-21 04:03:57 +0100

    sidebar update

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -59,7 +59,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
   return (
     <aside
       ref={sidebar}
-      className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0 ${
+      className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-white duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0 ${
         sidebarOpen ? 'translate-x-0' : '-translate-x-full'
       }`}
     >
@@ -67,7 +67,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
         <NavLink to="/">
           {/* <img src={Logo} alt="Logo" /> */}
-          <h1 className="font-bold text-white text-2xl">EDAS APP</h1>
+          <h1 className="font-bold text-black dark:text-white text-2xl">
+            EDAS APP
+          </h1>
         </NavLink>
 
         <button
@@ -183,7 +185,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                               to="/"
                               className={({ isActive }) =>
                                 'group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ' +
-                                (isActive && '!text-white')
+                                (isActive && 'text-black')
                               }
                             >
                               My Dashboard
